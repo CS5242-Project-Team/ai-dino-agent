daf21d39f4c1b65484f191dae2aec55c13a8efe6
Jaivignesh Venugopal <smugs1995@gmail.com>
2021-11-10 16:36:19 +0800

    model agent

---
+++ dino_agent.py
@@ -0,0 +1,78 @@
+import keyboard
+from PIL import Image
+import numpy as np
+from mss import mss
+import torch
+import pyautogui
+import os
+
+from model_architectures import MLP
+
+# -------------------------- CONFIG ---------------------------
+BINARY_MODEL_NAME = 'mlp.pt'
+IMAGE_SIZE = 255
+WIDTH, HEIGHT = pyautogui.size()
+BINARY_MODEL_PATH = os.path.join('binary_models', BINARY_MODEL_NAME)
+# -------------------------- END CONFIG ---------------------------
+
+
+# Assign model
+def get_model():
+    model = MLP()
+    # model = CNN()
+
+    return model
+
+
+# Image transformations
+def process_image(image):
+    # Resize image
+    new_size = (1366, 768)
+    image_data = image.resize(new_size)
+
+    # Crop image
+    crop_size = (0, 150, 500, 500)
+    image_data = image_data.crop(crop_size)
+
+    new_size = (IMAGE_SIZE, IMAGE_SIZE)
+    image_data = image_data.resize(new_size)
+    image_data = np.array(image_data.resize(new_size))
+
+    return image_data
+
+
+# A function for go up in the game
+def up():
+    keyboard.press_and_release(keyboard.KEY_UP)
+
+
+if __name__ == '__main__':
+    ss_manager = mss()
+    frame = {"top": 0, "left": 0, "width": WIDTH, "height": HEIGHT}
+
+    net = get_model()
+    net.load_state_dict(torch.load(BINARY_MODEL_PATH, map_location=torch.device('cpu')))
+    net.eval()
+
+    while True:
+        # Grab screenshot
+        ss = ss_manager.grab(frame)
+        image = Image.frombytes("RGB", ss.size, ss.rgb)
+        grey_image = image.convert("L")
+        transformed_image = process_image(grey_image)
+
+        X = torch.from_numpy(transformed_image)
+        X = X.view(1, -1)
+        X = X.float()
+
+        prediction = net(X)
+
+        result = np.argmax(prediction.detach().numpy())
+        print(result)
+        print("--------------------------")
+
+        if result == 0:  # go right
+            print("right")
+        elif result == 1:  # go up
+            up()
+            print("up")
---
+++ model_architectures.py
@@ -0,0 +1,184 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MLP(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(255 * 255 * 1, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 2)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class LeNet5_convnet(nn.Module):
+
+    def __init__(self):
+
+        super(LeNet5_convnet, self).__init__()
+
+        # CL1:   30 x 30  -->    50 x 28 x 28 
+        self.conv1 = nn.Conv2d(1,   50,  kernel_size=3,  padding=0 )
+        
+        # MP1: 50 x 28 x 28 -->    50 x 14 x 14
+        self.pool1  = nn.MaxPool2d(2,2)
+        
+        # CL2:   50 x 14 x 14  -->    100 x 14 x 14 
+        self.conv2 = nn.Conv2d(50,  100,  kernel_size=3,  padding=1 )
+        
+        # MP2: 100 x 14 x 14 -->    100 x 7 x 7
+        self.pool2 = nn.MaxPool2d(2,2)
+        
+        # LL1:   100 x 7 x 7 = 4900 -->  100 
+        self.linear1 = nn.Linear(4900, 100)
+        
+        # LL2:   100  -->  2 
+        self.linear2 = nn.Linear(100,2)
+
+
+    def forward(self, x):
+
+        # CL1:   30 x 30  -->    50 x 28 x 28 
+        x = self.conv1(x)
+        x = F.relu(x)
+        
+        # MP1: 50 x 28 x 28 -->    50 x 14 x 14
+        x = self.pool1(x)
+        
+        # CL2:   50 x 14 x 14  -->    100 x 14 x 14
+        x = self.conv2(x)
+        x = F.relu(x)
+        
+        # MP2: 100 x 14 x 14 -->    100 x 7 x 7
+        x = self.pool2(x)
+
+        # LL1:   100 x 7 x 7 = 4900  -->  100 
+        x = x.view(-1, 4900)
+        x = self.linear1(x)
+        x = F.relu(x)
+        
+        # LL2:   4900  -->  2 
+        x = self.linear2(x)
+    
+        return x
+
+class VGG_convnet(nn.Module):
+
+    def __init__(self):
+
+        super(VGG_convnet, self).__init__()
+
+        # block 1:         1 x 32 x 32 --> 64 x 16 x 16        
+        self.conv1a = nn.Conv2d(1,   64,  kernel_size=3, padding=1 )
+        self.conv1b = nn.Conv2d(64,  64,  kernel_size=3, padding=1 )
+        self.pool1  = nn.MaxPool2d(2,2)
+
+        # block 2:         64 x 16 x 16 --> 128 x 8 x 8
+        self.conv2a = nn.Conv2d(64,  128, kernel_size=3, padding=1 )
+        self.conv2b = nn.Conv2d(128, 128, kernel_size=3, padding=1 )
+        self.pool2  = nn.MaxPool2d(2,2)
+
+        # block 3:         128 x 8 x 8 --> 256 x 4 x 4        
+        self.conv3a = nn.Conv2d(128, 256, kernel_size=3, padding=1 )
+        self.conv3b = nn.Conv2d(256, 256, kernel_size=3, padding=1 )
+        self.pool3  = nn.MaxPool2d(2,2)
+        
+        #block 4:          256 x 4 x 4 --> 512 x 2 x 2
+        self.conv4a = nn.Conv2d(256, 512, kernel_size=3, padding=1 )
+        self.pool4  = nn.MaxPool2d(2,2)
+
+        # linear layers:   512 x 2 x 2 --> 2048 --> 4096 --> 4096 --> 2
+        self.linear1 = nn.Linear(2048, 4096)
+        self.linear2 = nn.Linear(4096,4096)
+        self.linear3 = nn.Linear(4096, 2)
+
+
+    def forward(self, x):
+
+        # block 1:         3 x 32 x 32 --> 64 x 16 x 16
+        x = self.conv1a(x)
+        x = F.relu(x)
+        x = self.conv1b(x)
+        x = F.relu(x)
+        x = self.pool1(x)
+
+        # block 2:         64 x 16 x 16 --> 128 x 8 x 8
+        x = self.conv2a(x)
+        x = F.relu(x)
+        x = self.conv2b(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+
+        # block 3:         128 x 8 x 8 --> 256 x 4 x 4
+        x = self.conv3a(x)
+        x = F.relu(x)
+        x = self.conv3b(x)
+        x = F.relu(x)
+        x = self.pool3(x)
+
+        #block 4:          256 x 4 x 4 --> 512 x 2 x 2
+        x = self.conv4a(x)
+        x = F.relu(x)
+        x = self.pool4(x)
+
+        # linear layers:   512 x 2 x 2 --> 2048 --> 4096 --> 4096 --> 2
+        x = x.view(-1, 2048)
+        x = self.linear1(x)
+        x = F.relu(x)
+        x = self.linear2(x)
+        x = F.relu(x)
+        x = self.linear3(x) 
+        
+        return x
+
+class Apple(nn.Module):
+
+    def __init__(self):
+
+        super(Apple, self).__init__()
+
+        # CL1:   102 x 102  -->    50 x 100 x 100 
+        self.conv1 = nn.Conv2d(1,   50,  kernel_size=3,  padding=0 )
+        
+        # MP1: 50 x 100 x 100 -->    50 x 50 x 50
+        self.pool1  = nn.MaxPool2d(2,2)
+        
+        # CL2:   50 x 50 x 50  -->    100 x 50 x 50 
+        self.conv2 = nn.Conv2d(50,  100,  kernel_size=3,  padding=1 )
+        
+        # MP2: 100 x 50 x 50 -->    100 x 25 x 25
+        self.pool2 = nn.MaxPool2d(2,2)
+
+        # LL1:   100 x 25 x 25 = 62500 -->  100 
+        self.linear1 = nn.Linear(62500, 100)
+        
+        # LL2:   100  -->  2 
+        self.linear2 = nn.Linear(100,2)
+
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = F.relu(x)
+
+        x = self.pool1(x)
+        
+        x = self.conv2(x)
+        x = F.relu(x)
+        
+        x = self.pool2(x)
+
+        x = x.view(-1, 4900)
+        x = self.linear1(x)
+        x = F.relu(x)
+        
+        x = self.linear2(x)
+    
+        return x
