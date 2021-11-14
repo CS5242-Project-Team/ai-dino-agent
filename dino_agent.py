15ec86cc66eba1a2ce11a42858161c7787ca794a
Jaivignesh Venugopal <smugs1995@gmail.com>
2021-11-15 00:13:28 +0800

    refactor

--- dino_agent.py
+++ dino_agent.py
@@ -2,28 +2,32 @@ import keyboard
 from PIL import Image
 import numpy as np
 from mss import mss
-import torch
 import pyautogui
 import os
 
-from model_architectures import MLP, LeNet5_convnet
+from model_architectures import *
 
 # -------------------------- CONFIG ---------------------------
-BINARY_MODEL_NAME = 'mlp.pt'
+IMAGE_SIZE = 32
 
-IMAGE_SIZE = 255
+# ARCHITECTURE = 'mlp'
+# ARCHITECTURE = 'cnn'
+ARCHITECTURE = 'rnn'
+# ARCHITECTURE = 'ann'
+
+# BINARY_MODEL_NAME = 'mlp.pt'
+# BINARY_MODEL_NAME = 'cnn.pt'
+# BINARY_MODEL_NAME = 'banana_v1.pt'
+BINARY_MODEL_NAME = 'vanilla_rnn_v2.pt'
+
+# MODEL = MLP()
+# MODEL = LeNet5_convnet()
+# MODEL = Banana()
+MODEL = VanillaRNN(input_size=IMAGE_SIZE*IMAGE_SIZE)
 # -------------------------- END CONFIG ---------------------------
 
 WIDTH, HEIGHT = pyautogui.size()
 BINARY_MODEL_PATH = os.path.join('binary_models', BINARY_MODEL_NAME)
-
-
-# Assign model
-def get_model():
-    model = MLP()
-    # model = LeNet5_convnet()
-
-    return model
 
 
 # Image transformations
@@ -48,11 +52,28 @@ def up():
     keyboard.press_and_release(keyboard.KEY_UP)
 
 
+def get_prediction(X):
+    if ARCHITECTURE == 'mlp':
+        X = X.view(1, -1).float()
+        return net(X)
+    elif ARCHITECTURE == 'cnn':
+        X = X.view(1, 1, IMAGE_SIZE, IMAGE_SIZE).float()
+        return net(X)
+    elif ARCHITECTURE == 'rnn':
+        X = X.view(-1, 1, IMAGE_SIZE * IMAGE_SIZE).float()
+        h = torch.zeros(1, 1, IMAGE_SIZE * IMAGE_SIZE).float()
+        scores, _ = net(X, h)
+        return scores
+    elif ARCHITECTURE == 'ann':
+        pass
+
+
 if __name__ == '__main__':
     ss_manager = mss()
     frame = {"top": 0, "left": 0, "width": WIDTH, "height": HEIGHT}
 
-    net = get_model()
+    # Load model
+    net = MODEL
     net.load_state_dict(torch.load(BINARY_MODEL_PATH, map_location=torch.device('cpu')))
     net.eval()
 
@@ -63,18 +84,15 @@ if __name__ == '__main__':
         grey_image = image.convert("L")
         transformed_image = process_image(grey_image)
 
+        # Predict
         X = torch.from_numpy(transformed_image)
-        X = X.view(1, -1)
-        X = X.float()
-
-        prediction = net(X)
-
+        prediction = get_prediction(X)
         result = np.argmax(prediction.detach().numpy())
-        print(result)
-        print("--------------------------")
 
+        # Take action
         if result == 0:  # go right
             print("right")
         elif result == 1:  # go up
             up()
             print("up")
+        print("--------------------------")
--- model_architectures.py
+++ model_architectures.py
@@ -1,12 +1,18 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
+#################################### MLP ##############################################
 class MLP(nn.Module):
+    '''
+      Multilayer Perceptron.
+    '''
+
     def __init__(self, ):
         super().__init__()
         self.layers = nn.Sequential(
             nn.Flatten(),
+            # nn.Linear(255 * 255 * 3, 64),
             nn.Linear(255 * 255 * 1, 64),
             nn.ReLU(),
             nn.Linear(64, 32),
@@ -15,93 +21,88 @@ class MLP(nn.Module):
         )
 
     def forward(self, x):
+        '''Forward pass'''
         return self.layers(x)
-
-
+#######################################################################################
+#################################### CNN ##############################################
 class LeNet5_convnet(nn.Module):
-
     def __init__(self):
-
         super(LeNet5_convnet, self).__init__()
 
-        # CL1:   30 x 30  -->    50 x 28 x 28 
-        self.conv1 = nn.Conv2d(1,   50,  kernel_size=3,  padding=0 )
-        
-        # MP1: 50 x 28 x 28 -->    50 x 14 x 14
-        self.pool1  = nn.MaxPool2d(2,2)
-        
-        # CL2:   50 x 14 x 14  -->    100 x 14 x 14 
-        self.conv2 = nn.Conv2d(50,  100,  kernel_size=3,  padding=1 )
-        
-        # MP2: 100 x 14 x 14 -->    100 x 7 x 7
-        self.pool2 = nn.MaxPool2d(2,2)
-        
-        # LL1:   100 x 7 x 7 = 4900 -->  100 
-        self.linear1 = nn.Linear(4900, 100)
-        
-        # LL2:   100  -->  2 
-        self.linear2 = nn.Linear(100,2)
+        # CL1:   30 x 30  -->    50 x 28 x 28
+        self.conv1 = nn.Conv2d(1, 50, kernel_size=3, padding=0)
 
+        # MP1: 50 x 28 x 28 -->    50 x 14 x 14
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        # CL2:   50 x 14 x 14  -->    100 x 14 x 14
+        self.conv2 = nn.Conv2d(50, 100, kernel_size=3, padding=1)
+
+        # MP2: 100 x 14 x 14 -->    100 x 7 x 7
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        # LL1:   100 x 7 x 7 = 4900 -->  100
+        self.linear1 = nn.Linear(4900, 100)
+
+        # LL2:   100  -->  2
+        self.linear2 = nn.Linear(100, 2)
 
     def forward(self, x):
-
-        # CL1:   30 x 30  -->    50 x 28 x 28 
+        # CL1:   30 x 30  -->    50 x 28 x 28
         x = self.conv1(x)
         x = F.relu(x)
-        
+
         # MP1: 50 x 28 x 28 -->    50 x 14 x 14
         x = self.pool1(x)
-        
+
         # CL2:   50 x 14 x 14  -->    100 x 14 x 14
         x = self.conv2(x)
         x = F.relu(x)
-        
+
         # MP2: 100 x 14 x 14 -->    100 x 7 x 7
         x = self.pool2(x)
 
-        # LL1:   100 x 7 x 7 = 4900  -->  100 
+        # LL1:   100 x 7 x 7 = 4900  -->  100
         x = x.view(-1, 4900)
         x = self.linear1(x)
         x = F.relu(x)
-        
-        # LL2:   4900  -->  2 
+
+        # LL2:   4900  -->  2
         x = self.linear2(x)
-    
+
         return x
+
 
 class VGG_convnet(nn.Module):
 
     def __init__(self):
-
         super(VGG_convnet, self).__init__()
 
-        # block 1:         1 x 32 x 32 --> 64 x 16 x 16        
-        self.conv1a = nn.Conv2d(1,   64,  kernel_size=3, padding=1 )
-        self.conv1b = nn.Conv2d(64,  64,  kernel_size=3, padding=1 )
-        self.pool1  = nn.MaxPool2d(2,2)
+        # block 1:         1 x 32 x 32 --> 64 x 16 x 16
+        self.conv1a = nn.Conv2d(1, 64, kernel_size=3, padding=1)
+        self.conv1b = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.pool1 = nn.MaxPool2d(2, 2)
 
         # block 2:         64 x 16 x 16 --> 128 x 8 x 8
-        self.conv2a = nn.Conv2d(64,  128, kernel_size=3, padding=1 )
-        self.conv2b = nn.Conv2d(128, 128, kernel_size=3, padding=1 )
-        self.pool2  = nn.MaxPool2d(2,2)
+        self.conv2a = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv2b = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool2d(2, 2)
 
-        # block 3:         128 x 8 x 8 --> 256 x 4 x 4        
-        self.conv3a = nn.Conv2d(128, 256, kernel_size=3, padding=1 )
-        self.conv3b = nn.Conv2d(256, 256, kernel_size=3, padding=1 )
-        self.pool3  = nn.MaxPool2d(2,2)
-        
-        #block 4:          256 x 4 x 4 --> 512 x 2 x 2
-        self.conv4a = nn.Conv2d(256, 512, kernel_size=3, padding=1 )
-        self.pool4  = nn.MaxPool2d(2,2)
+        # block 3:         128 x 8 x 8 --> 256 x 4 x 4
+        self.conv3a = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.conv3b = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.pool3 = nn.MaxPool2d(2, 2)
+
+        # block 4:          256 x 4 x 4 --> 512 x 2 x 2
+        self.conv4a = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.pool4 = nn.MaxPool2d(2, 2)
 
         # linear layers:   512 x 2 x 2 --> 2048 --> 4096 --> 4096 --> 2
         self.linear1 = nn.Linear(2048, 4096)
-        self.linear2 = nn.Linear(4096,4096)
+        self.linear2 = nn.Linear(4096, 4096)
         self.linear3 = nn.Linear(4096, 2)
 
-
     def forward(self, x):
-
         # block 1:         3 x 32 x 32 --> 64 x 16 x 16
         x = self.conv1a(x)
         x = F.relu(x)
@@ -123,7 +124,7 @@ class VGG_convnet(nn.Module):
         x = F.relu(x)
         x = self.pool3(x)
 
-        #block 4:          256 x 4 x 4 --> 512 x 2 x 2
+        # block 4:          256 x 4 x 4 --> 512 x 2 x 2
         x = self.conv4a(x)
         x = F.relu(x)
         x = self.pool4(x)
@@ -134,51 +135,103 @@ class VGG_convnet(nn.Module):
         x = F.relu(x)
         x = self.linear2(x)
         x = F.relu(x)
-        x = self.linear3(x) 
-        
+        x = self.linear3(x)
+
         return x
+
 
 class Apple(nn.Module):
 
     def __init__(self):
-
         super(Apple, self).__init__()
 
-        # CL1:   102 x 102  -->    50 x 100 x 100 
-        self.conv1 = nn.Conv2d(1,   50,  kernel_size=3,  padding=0 )
-        
+        # CL1:   102 x 102  -->    50 x 100 x 100
+        self.conv1 = nn.Conv2d(1, 50, kernel_size=3, padding=0)
+
         # MP1: 50 x 100 x 100 -->    50 x 50 x 50
-        self.pool1  = nn.MaxPool2d(2,2)
-        
-        # CL2:   50 x 50 x 50  -->    100 x 50 x 50 
-        self.conv2 = nn.Conv2d(50,  100,  kernel_size=3,  padding=1 )
-        
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        # CL2:   50 x 50 x 50  -->    100 x 50 x 50
+        self.conv2 = nn.Conv2d(50, 100, kernel_size=3, padding=1)
+
         # MP2: 100 x 50 x 50 -->    100 x 25 x 25
-        self.pool2 = nn.MaxPool2d(2,2)
+        self.pool2 = nn.MaxPool2d(2, 2)
 
-        # LL1:   100 x 25 x 25 = 62500 -->  100 
-        self.linear1 = nn.Linear(62500, 100)
-        
-        # LL2:   100  -->  2 
-        self.linear2 = nn.Linear(100,2)
+        # LL1:   100 x 25 x 25 = 62500 -->  100
+        self.linear1 = nn.Linear(100 * 25 * 25, 100)
 
+        # LL2:   100  -->  2
+        self.linear2 = nn.Linear(100, 2)
 
     def forward(self, x):
-
         x = self.conv1(x)
         x = F.relu(x)
-
         x = self.pool1(x)
-        
+
         x = self.conv2(x)
         x = F.relu(x)
-        
         x = self.pool2(x)
 
-        x = x.view(-1, 4900)
+        x = x.view(-1, 100 * 25 * 25)
         x = self.linear1(x)
         x = F.relu(x)
-        
+
         x = self.linear2(x)
-    
+
         return x
+
+
+class Banana(nn.Module):
+
+    def __init__(self):
+        super(Banana, self).__init__()
+
+        # CL1:   51 x 51  -->    100 x 50 x 50
+        self.conv1 = nn.Conv2d(1, 100, kernel_size=2, padding=0)
+
+        # MP1: 100 x 50 x 50 -->    100 x 25 x 25
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        # CL2:   100 x 25 x 25  -->    200 x 26 x 26
+        self.conv2 = nn.Conv2d(100, 200, kernel_size=2, padding=1)
+
+        # MP2: 200 x 26 x 26 -->    200 x 13 x 13
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        # LL1:   200 x 13 x 13  -->  1000
+        self.linear1 = nn.Linear(200 * 13 * 13, 1000)
+
+        # LL2:   1000  -->  2
+        self.linear2 = nn.Linear(1000, 2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+
+        x = x.view(-1, 200 * 13 * 13)
+        x = self.linear1(x)
+        x = F.relu(x)
+
+        x = self.linear2(x)
+
+        return x
+#######################################################################################
+#################################### RNN ##############################################
+class VanillaRNN(nn.Module):
+    def __init__(self, input_size):
+        super(VanillaRNN, self).__init__()
+        self.layers = 1
+        self.input_size = input_size
+        self.layer1 = nn.RNN(input_size, input_size, self.layers, batch_first=True)
+        self.layer2 = nn.Linear(input_size, 2)
+
+    def forward(self, x, h_init):
+        h_seq, h_final = self.layer1(x, h_init)
+        score_seq = self.layer2(h_seq)
+        score_seq = score_seq[:, -1]
+        return score_seq, h_final
