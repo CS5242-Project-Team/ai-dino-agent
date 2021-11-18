bd01313c112fae7d4905df0bcac6ace49e651865
Jaivignesh Venugopal <smugs1995@gmail.com>
2021-11-18 12:29:23 +0800

    remove unnecessary files

--- change_name.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for file in os.listdir('.'):
-	if '1_' in file:
-		file_name = file.split('.')[0]
-		_, id1, id2 = file_name.split('_')
-		os.rename(file, '{}_{}_{}.jpg'.format(2, id1, id2))
